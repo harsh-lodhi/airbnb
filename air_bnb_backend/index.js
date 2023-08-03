c7c93038ffdcd7a9c1a05b54075dbc612e7c32b4
Harsh Lodhi <harshlodhi0809@gmail.com>
2023-08-04 01:54:32 +0530

    cors edited cors policy

--- air_bnb_backend/index.js
+++ air_bnb_backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'https://airbnb-one-black.vercel.app/',
+  origin: 'https://airbnb-one-black.vercel.app',
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
